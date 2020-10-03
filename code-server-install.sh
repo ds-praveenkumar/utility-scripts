6edc955b961eefa2cdfc73588fd52b0297caff45
ds-praveenkumar <praveenkr2208@gmail.com>
2020-10-04 00:27:30 +0530

    update code server install script

--- code-server-install.sh
+++ code-server-install.sh
@@ -1,1 +1,3 @@
-curl -fsSL https://code-server.dev/install.sh | sh
+curl -fOL https://github.com/cdr/code-server/releases/download/v3.5.0/code-server_3.5.0_amd64.deb
+sudo dpkg -i code-server_3.5.0_amd64.deb
+sudo systemctl enable --now code-server@$USER
